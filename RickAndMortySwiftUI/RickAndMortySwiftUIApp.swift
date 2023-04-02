398e1fe9801a4fa6d6fe73bc6cb3d3958a6577d2
Casey Barth <cjbarth1@gmail.com>
2023-04-02 16:29:14 -0700

    Add base project

---
+++ RickAndMortySwiftUI/RickAndMortySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortySwiftUIApp.swift
+//  RickAndMortySwiftUI
+//
+//  Created by Casey Barth on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
